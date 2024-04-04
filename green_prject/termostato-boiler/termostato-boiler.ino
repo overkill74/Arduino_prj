e79981000f49b712b951d3956d3ecb905240602e
***** By The Overkill ***** <overkill.shining@tiscali.it>
2024-04-04 22:58:20 +0200

    Aggiunto OTA per termostato boiler

--- green_prject/termostato-boiler/termostato-boiler.ino
+++ green_prject/termostato-boiler/termostato-boiler.ino
@@ -3,24 +3,24 @@
 #include "twat.h"
 #include "web-time.h"
 #include_next <Ticker.h>
+#include <ArduinoOTA.h>
 #include <EEPROM.h>
 
 
 
-class StoredData
-{
+class StoredData {
 private:
   class StoreVariables {
-    public:
-      float     m_set_point = 35;         //! Set point temperatura
-      float     m_set_point_doccia = 45;  //! Set point temperatura doccia
-      uint32_t  m_energy = 0;             //! Energy
-      union Flags {
-        struct {
-          uint32_t    is_dst  : 1;        //! Is Daylight saving time
-        } b;
-        uint32_t all;
-      } flags;
+  public:
+    float m_set_point = 35;         //! Set point temperatura
+    float m_set_point_doccia = 45;  //! Set point temperatura doccia
+    uint32_t m_energy = 0;          //! Energy
+    union Flags {
+      struct {
+        uint32_t is_dst : 1;  //! Is Daylight saving time
+      } b;
+      uint32_t all;
+    } flags;
   };
 
 public:
@@ -33,34 +33,32 @@ public:
   }
 
   ///
-  /// \brief 
+  /// \brief
   float incSetPoint() {
     if (m_stored_vars.m_set_point >= m_max_temp) {
       m_stored_vars.m_set_point = m_max_temp;
-    }
-    else {
+    } else {
       m_stored_vars.m_set_point += 1;
     }
     store_data();
     return m_stored_vars.m_set_point;
   }
   ///
-  /// \brief 
+  /// \brief
   float incSetPointDoccia() {
     if (m_stored_vars.m_set_point_doccia >= m_max_temp) {
       m_stored_vars.m_set_point_doccia = m_max_temp;
-    }
-    else {
+    } else {
       m_stored_vars.m_set_point_doccia += 1;
     }
     store_data();
     return m_stored_vars.m_set_point_doccia;
   }
   ///
-  /// \brief 
+  /// \brief
   uint32_t incEnergy(uint32_t delta) {
     m_stored_vars.m_energy += delta / 100;
-    if (++m_cnt_store_energy >= 5*60) {
+    if (++m_cnt_store_energy >= 5 * 60) {
       store_data();
     }
     // Serial.print("Energy : ");
@@ -70,64 +68,62 @@ public:
     return m_stored_vars.m_energy;
   }
   ///
-  /// \brief 
+  /// \brief
   void saveEnergy() {
     store_data();
   }
   ///
-  /// \brief 
+  /// \brief
   void resetEnergy() {
     m_stored_vars.m_energy = 0;
     store_data();
   }
 
   ///
-  /// \brief 
+  /// \brief
   float decSetPoint() {
     if (m_stored_vars.m_set_point <= m_min_temp) {
       m_stored_vars.m_set_point = m_min_temp;
-    }
-    else {
+    } else {
       m_stored_vars.m_set_point -= 1;
     }
     store_data();
     return m_stored_vars.m_set_point;
   }
   ///
-  /// \brief 
+  /// \brief
   float decSetPointDoccia() {
     if (m_stored_vars.m_set_point_doccia <= m_min_temp) {
       m_stored_vars.m_set_point_doccia = m_min_temp;
-    }
-    else {
+    } else {
       m_stored_vars.m_set_point_doccia -= 1;
     }
     store_data();
     return m_stored_vars.m_set_point_doccia;
   }
   ///
-  /// \brief 
+  /// \brief
   bool toggleIsDst() {
     m_stored_vars.flags.b.is_dst ^= 1;
     return m_stored_vars.flags.b.is_dst;
   }
   ///
-  /// \brief 
+  /// \brief
   float getSetPoint() {
     return m_stored_vars.m_set_point;
   }
   ///
-  /// \brief 
+  /// \brief
   float getSetPointDoccia() {
     return m_stored_vars.m_set_point_doccia;
   }
   ///
-  /// \brief 
+  /// \brief
   float getEnergy() {
     return (float)(((m_stored_vars.m_energy + 500) / 10) / 3600.0f) * m_r_pow;
   }
   ///
-  /// \brief 
+  /// \brief
   bool isDst() {
     return m_stored_vars.flags.b.is_dst;
   }
@@ -152,12 +148,12 @@ private:
     if (isnan(m_stored_vars.m_set_point)) m_stored_vars.m_set_point = 35;
     if (isnan(m_stored_vars.m_set_point_doccia)) m_stored_vars.m_set_point_doccia = 45;
     if (m_stored_vars.m_energy == 0xFFFFFFFF) m_stored_vars.m_energy = 0;
-    
+
     if (m_stored_vars.m_set_point < m_min_temp) m_stored_vars.m_set_point = 35;
     if (m_stored_vars.m_set_point > m_max_temp) m_stored_vars.m_set_point = 35;
     if (m_stored_vars.m_set_point_doccia < m_min_temp) m_stored_vars.m_set_point_doccia = 45;
     if (m_stored_vars.m_set_point_doccia > m_max_temp) m_stored_vars.m_set_point_doccia = 45;
-/*
+    /*
     Serial.print("m_set_point        : "); Serial.println(m_set_point); 
     Serial.print("m_set_point_doccia : "); Serial.println(m_set_point_doccia); 
     Serial.print("m_max_temp         : "); Serial.println(m_max_temp); 
@@ -166,19 +162,19 @@ private:
   }
 
 private:
-  StoreVariables  m_stored_vars;  //! Store variables
+  StoreVariables m_stored_vars;  //! Store variables
 
-  int m_cnt_store_energy = 0;     //! Counter to store updated energy
-  const float m_max_temp = 55;    //! Massimo setpoint
-  const float m_min_temp = 5;     //! Minimo setpoint
-  const float m_r_pow = 1200;     //! Power consumed by resistance
+  int m_cnt_store_energy = 0;   //! Counter to store updated energy
+  const float m_max_temp = 55;  //! Massimo setpoint
+  const float m_min_temp = 5;   //! Minimo setpoint
+  const float m_r_pow = 1200;   //! Power consumed by resistance
 };
 
 
 // Dichiarazioni delle costanti per il LED e il bottone
-#define SENSOR_DATA D4 // - Sensore Data
-#define LED_PIN     D6 // - Led
-#define RELE_PIN    D5 // - RELE
+#define SENSOR_DATA D4  // - Sensore Data
+#define LED_PIN D6      // - Led
+#define RELE_PIN D5     // - RELE
 
 // Dichiarazioni delle costanti per la connessione WiFi
 const char* ssid = "FASTWEB-9n9CGH";
@@ -199,15 +195,14 @@ bool g_connected = false;
 bool g_temp_ok = false;
 bool g_out_rele = false;
 
-float g_water_temp = 20;          //! Temperatura dell'acqua
+float g_water_temp = 20;  //! Temperatura dell'acqua
 
 bool g_is_doccia_scheduled = false;
 uint32_t g_tout_ch_doccia = 0;
 
 //---------------------------------------------------------
 /// \brief ricarica la pagina
-static void reload_page()
-{
+static void reload_page() {
   server.sendHeader("Location", "/");
   server.send(302, "text/plain", "");
 
@@ -216,99 +211,89 @@ static void reload_page()
 }
 
 /// \brief Incrementa il setpoint
-void setpointInc()
-{
+void setpointInc() {
   g_store_data.incSetPoint();
   reload_page();
 }
 
 /// \brief Decrementa il setpoint
-void setpointDec()
-{
+void setpointDec() {
   g_store_data.decSetPoint();
   reload_page();
 }
 
 /// \brief Incrementa il setpoint doccia
-void setpointDocciaInc()
-{
+void setpointDocciaInc() {
   g_store_data.incSetPointDoccia();
   reload_page();
 }
 
 /// \brief Decrementa il setpoint doccia
-void setpointDocciaDec()
-{
+void setpointDocciaDec() {
   g_store_data.decSetPointDoccia();
   reload_page();
 }
 
 /// \brief Prenota la doccia
-void prenotaDocciaSet()
-{
+void prenotaDocciaSet() {
   g_is_doccia_scheduled = true;
   g_tout_ch_doccia = millis() + 2 * 3600 * 1000;
   reload_page();
 }
 
 /// \brief Cancella la prenotazione
-void prenotaDocciaClr()
-{
+void prenotaDocciaClr() {
   g_is_doccia_scheduled = false;
   g_tout_ch_doccia = 0;
   reload_page();
 }
 
 /// \brief Cancella la prenotazione
-void resetEnergy()
-{
+void resetEnergy() {
   g_store_data.resetEnergy();
   reload_page();
 }
 
 /// \brief Toggle DST
-void toggleDst()
-{
+void toggleDst() {
   g_web_time.setDst(g_store_data.toggleIsDst());
   reload_page();
 }
 
 // Funzione per la gestione della richiesta HTTP GET
-void handleRoot()
-{
+void handleRoot() {
   String html = "<html>"
-  "<head>"
-  "<title>Boiler del bagno</title>"
-  "<meta http-equiv=\"refresh\" content=\"5\">"
-  "<style>"
-  "  table {"
-  "    border-collapse: separate;"
-  "    border-spacing: 10px;"
-  //"    background-color: lightgray;"
-  "    text-align: left;"
-  //"    border: 1px solid black;"
-  "    font-size: 25px;"
-  "  }"
-  "  tr {"
-  "    height: 30px;"
-  "  }"
-  "  td {"
-  "    padding: 0px 0px 0px 30px ;"
-  "  }"
-  "  td:first-child {"
-  "    padding-left: 0;"
-  "  }"
-  "</style>"
-  "</head>"
-  "<body>"
-  "<h1>Boiler del bagno</h1>";
+                "<head>"
+                "<title>Boiler del bagno</title>"
+                "<meta http-equiv=\"refresh\" content=\"5\">"
+                "<style>"
+                "  table {"
+                "    border-collapse: separate;"
+                "    border-spacing: 10px;"
+                //"    background-color: lightgray;"
+                "    text-align: left;"
+                //"    border: 1px solid black;"
+                "    font-size: 25px;"
+                "  }"
+                "  tr {"
+                "    height: 30px;"
+                "  }"
+                "  td {"
+                "    padding: 0px 0px 0px 30px ;"
+                "  }"
+                "  td:first-child {"
+                "    padding-left: 0;"
+                "  }"
+                "</style>"
+                "</head>"
+                "<body>"
+                "<h1>Boiler del bagno</h1>";
 
   html += "<table><tr>";
   html += "<td><b>Ora: " + g_web_time.getDateTime() + "</b></td>";
   if (g_store_data.isDst()) {
     html += "<td><button onclick=\"location.href='/toggle_dst'\">Clear DST</button></td>";
-  }
-  else {
+  } else {
     html += "<td><button onclick=\"location.href='/toggle_dst'\">Set DST</button></td>";
   }
   html += "</tr></table>";
@@ -322,8 +307,7 @@ void handleRoot()
   dtostrf(g_store_data.getSetPoint(), 4, 1, txt);
   if (!g_is_doccia_scheduled) {
     html += "<h3><font color=\"green\">&rarr;";
-  }
-  else {
+  } else {
     html += "<h3><font color=\"black\">";
   }
   html += "Set Point : " + String(txt) + "&deg;C  </font>";
@@ -334,8 +318,7 @@ void handleRoot()
   dtostrf(g_store_data.getSetPointDoccia(), 4, 1, txt);
   if (g_is_doccia_scheduled) {
     html += "<h3><font color=\"green\">&rarr;";
-  }
-  else {
+  } else {
     html += "<h3><font color=\"black\">";
   }
   html += "Set Point Doccia : " + String(txt) + "&deg;C  </font>";
@@ -346,8 +329,7 @@ void handleRoot()
   if (g_is_doccia_scheduled) {
     html += "<hr><h3><font color=\"red\">Doccia Prenotata   </font>";
     html += "<button onclick=\"location.href='/sch_doccia_clr'\">Annulla</button></h3>";
-  }
-  else {
+  } else {
     html += "<hr><h3><font color=\"blue\">Doccia non prenotata   </font>";
     html += "<button onclick=\"location.href='/sch_doccia_set'\">PRENOTA</button></h3>";
   }
@@ -357,25 +339,23 @@ void handleRoot()
   html += g_out_rele ? "<mark>Acceso</mark></p>" : "Spento</p><hr>";
 
   // Power consumption
-  dtostrf((float)g_store_data.getEnergy() / 1000.0f , 4, 2, txt);
+  dtostrf((float)g_store_data.getEnergy() / 1000.0f, 4, 2, txt);
   html += "<h3>Energia consumata : " + String(txt) + "kWh    ";
   html += "<button onclick=\"location.href='/reset_energy'\">RESET</button>";
-  
+
   server.send(200, "text/html", html);
   server_ext.send(200, "text/html", html);
 }
 
 
-static bool handle_connection()
-{
-    // Gestione connessione
+static bool handle_connection() {
+  // Gestione connessione
   static bool connected = false;
 
   if (WiFi.status() != WL_CONNECTED) {
     connected = false;
     Serial.println("Connessione in corso...");
-  }
-  else {
+  } else {
     if (!connected) {
       // Stampa l'indirizzo IP sulla seriale
       Serial.print("Indirizzo IP: ");
@@ -387,10 +367,9 @@ static bool handle_connection()
   return connected;
 }
 
-static bool ctrl_temperature()
-{
+static bool ctrl_temperature() {
   static uint32_t t_out = 0;
-  
+
   if (g_is_doccia_scheduled) {
     if (millis() > g_tout_ch_doccia) {
       g_tout_ch_doccia = 0;
@@ -398,7 +377,7 @@ static bool ctrl_temperature()
     }
   }
   float sp = g_is_doccia_scheduled ? g_store_data.getSetPointDoccia() : g_store_data.getSetPoint();
-  
+
   // Legge la temperatura
   float temp = g_twat.readTemperature();
   if (temp <= T_ERRROR) {
@@ -415,40 +394,35 @@ static bool ctrl_temperature()
       g_out_rele = false;
       g_store_data.saveEnergy();
     }
-  }
-  else {
+  } else {
     // Uscita spenta
     if (g_water_temp <= (sp - 2)) {
       g_out_rele = true;
       t_out = millis();
     }
   }
-  
+
   digitalWrite(RELE_PIN, g_out_rele);
 
   return true;
 }
 
-static void ctrl_led()
-{
+static void ctrl_led() {
   static int32_t tout_led = 0;
   static bool led_phase = false;
   static int missing_cnt_tout = 10;
 
-  if (--tout_led <= 0)
-  {
+  if (--tout_led <= 0) {
     if (!g_temp_ok) {
       // Lampeggio Errore
       led_phase ^= true;
       tout_led = 10;
-    }
-    else {
+    } else {
       if (!g_connected) {
         // Lampeggio mancata connessione
         led_phase ^= true;
         tout_led = 50;
-      }
-      else {
+      } else {
         led_phase = g_out_rele;
       }
     }
@@ -457,8 +431,7 @@ static void ctrl_led()
   digitalWrite(LED_PIN, led_phase);
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   // Setup
@@ -500,17 +473,55 @@ void setup()
 
   // Connessione alla rete WiFi
   WiFi.begin(ssid, password);
-  
+
   // Imposta DST
   g_web_time.setDst(g_store_data.isDst());
+
+  // OTA
+  ArduinoOTA.setPassword("Z0rba51lgr3c0!");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+    //is_firmwup_run = true;
+    //is_firmwup_error = false;
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+    //is_firmwup_run = false;
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    //is_firmwup_run = false;
+    //is_firmwup_error = true;
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
-void loop()
-{
+void loop() {
   static uint32_t tout = 0;
 
-  if (millis() > tout)
-  {
+  if (millis() > tout) {
     tout = millis() + 1000;
 
     g_connected = handle_connection();
@@ -523,7 +534,7 @@ void loop()
   server_ext.handleClient();
 
   // Controllo dello stato del bottone
-/*
+  /*
   if (digitalRead(BUTTON_PIN) == LOW) {
     delay(50); // Debounce
     if (digitalRead(BUTTON_PIN) == LOW) {
@@ -531,5 +542,7 @@ void loop()
     }
   }
   */
-}
 
+  // OTA
+  ArduinoOTA.handle();
+}
--- sysco/SyscoTermostato/SyscoTermostato.ino
+++ sysco/SyscoTermostato/SyscoTermostato.ino
@@ -5,24 +5,127 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#include <AceCRC.h>
+using namespace ace_crc::crc16ccitt_nibble;
+
 // Display
 //#include "Display.h"
 
-//--------------------------------------------------------------------------------
-OneWire oneWire(A0); 
-DallasTemperature sensore(&oneWire);
+#define UART_BUFFER_SIZE 128
 
 //--------------------------------------------------------------------------------
-void setup()
+OneWire oneWire(A0);
+DallasTemperature sensore(&oneWire);
+
+static uint8_t m_modbus_addr = 1;
+
+union ModbusRegisters {
+  struct regs {
+    uint16_t on_off;        //  1 Acceso / spento 1=on
+    uint16_t rfu_2;         //  2
+    uint16_t working_mode;  //  3 Modalita' funzionamento 0=Cool, 1=Heat, 2=vent
+    uint16_t temp_setpont;  //  4 Setpoint temperatura ambiente
+    uint16_t key_lock;      //  5 Blocco tasti 1=locked
+    uint16_t rfu_6;         //  6
+    uint16_t rfu_7;         //  7
+    uint16_t rfu_8;         //  8
+    uint16_t temp_amb;      //  9 Temperatura ambiente
+    uint16_t rfu_10;        //  10
+  } regs;
+  uint16_t words[10];
+};
+typedef union ModbusRegisters ModbusRegisters;
+
+static ModbusRegisters m_registers;
+
+//--------------------------------------------------------------------------------
+static uint16_t swapEndian(uint16_t value) {
+    return (value >> 8) | (value << 8);
+}
+
+//--------------------------------------------------------------------------------
+static bool modbus_read_coils(const char* buffer) {
+  uint16_t adr = ((uint16_t)buffer[2] << 8) | buffer[3];
+  uint16_t num = ((uint16_t)buffer[4] << 8) | buffer[5];
+  uint16_t crc = ((uint16_t)buffer[6] << 8) | buffer[7];
+
+  uint16_t modbus_ans[16];
+  size_t modbus_ans_sz = 0;
+
+  for (uint16_t i=0; i<num; ++i) {
+    modbus_ans[i+1] = swapEndian(adr + i); //m_registers.words[adr + i];
+  }
+
+  modbus_ans[0] = m_modbus_addr | (uint16_t)(((num * 2)) << 8);
+
+  crc_t crc16 = crc_init();
+  crc16 = crc_update(crc16, modbus_ans, num * 2 + 2);
+  crc16 = crc_finalize(crc16);
+  modbus_ans[num] = swapEndian(0x55AA);
+
+  Serial.write((const char*)modbus_ans, num * 2 + 4);
+}
+
+//--------------------------------------------------------------------------------
+static bool modbus_parser(const char* buffer, size_t buffer_sz)
 {
+  if (buffer_sz < 8) {
+    return false;
+  }
+  size_t max_len = buffer_sz - 8;
+  for (size_t x=0; x<=max_len; ++x)
+  {
+    if (buffer[x] == m_modbus_addr)
+    {
+      if (buffer[x + 1] == 3) {
+        modbus_read_coils(&buffer[x]);
+      }
+      return true;
+    }
+  }
+  return false;
+}
+
+//--------------------------------------------------------------------------------
+static void modbus_receive()
+{
+  static char message[UART_BUFFER_SIZE];
+  static size_t message_pos = 0;
+  while (Serial.available() > 0) {
+    if (message_pos >= UART_BUFFER_SIZE) {
+      // Overflow
+      break;
+    }
+    message[message_pos++] = Serial.read();
+    delay(25);
+  }
+
+  // if (message_pos) {
+  //   // Serial.println(message_pos);
+  //    Serial.write((const char*)&message_pos, 2);
+  //    Serial.write(message, message_pos);
+
+  // }
+
+  // Parser
+  if (modbus_parser(message, message_pos)) {
+    // Parser OK
+  }
+
+  message_pos = 0;
+}
+
+//--------------------------------------------------------------------------------
+void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);  // Tx
 
   //--------------------------------------
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Welcome to Sysco Thermostat");
+  while (!Serial)
+    ;
+  //Serial.println("Welcome to Sysco Thermostat");
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);  // Tx
 
@@ -46,16 +149,17 @@ void setup()
   digitalWrite(LED_BUILTIN, LOW);  // Tx
 }
 //--------------------------------------------------------------------------------
-void loop()
-{
+void loop() {
   static uint32_t onesec = 0;
   static int delay_send = 5;
-  while (millis() < onesec);
-  onesec+=1000;
+  while (millis() < onesec) {
+      modbus_receive();
+  }
+  onesec += 1000;
   //--------------------------------------
   // Read Setpoint
   int val = analogRead(A2);
-  float setpoint = 15.0f + (15.0f/1020.0f) * (float)val;
+  float setpoint = 15.0f + (15.0f / 1020.0f) * (float)val;
   static float old_setpoint = -1.0f;
 
   if (fabs(old_setpoint - setpoint) > 2) {
@@ -73,6 +177,8 @@ void loop()
   sensore.requestTemperatures();
   float temperature = sensore.getTempCByIndex(0) - 3.0f;
 
+
+  /*
   String s = String((int)(setpoint*100.0f));
   String t = String((int)(temperature*100.0f));
   while(s.length() < 6) { s='0' + s; }
@@ -93,12 +199,10 @@ void loop()
   digitalWrite(2, LOW);  // Tx
   digitalWrite(LED_BUILTIN, LOW);  // Tx
   //return;
-
+*/
   // SyscoDisplay* dysp = SyscoDisplay::getInstance();
   // dysp->updateBegin();
   // dysp->drawSetPoint(setpoint);
   // dysp->drawTemperature(temperature);
   // dysp->updateEnd();
-
 }
-
