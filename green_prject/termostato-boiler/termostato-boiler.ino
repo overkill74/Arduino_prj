76fd4b9cf09800a1de8fceb0455ab50ae8e7ebda
***** By The Overkill ***** <overkill.shining@tiscali.it>
2024-06-04 01:03:55 +0200

    Migliorata pagina boiler

--- green_prject/termostato-boiler/termostato-boiler.ino
+++ green_prject/termostato-boiler/termostato-boiler.ino
@@ -6,7 +6,7 @@
 #include <ArduinoOTA.h>
 #include <EEPROM.h>
 
-
+#define VERSION "1.02.00"
 
 class StoredData {
 private:
@@ -34,22 +34,20 @@ public:
 
   ///
   /// \brief
-  float incSetPoint() {
+  float incSetPoint(int val=1) {
+    m_stored_vars.m_set_point += val;
     if (m_stored_vars.m_set_point >= m_max_temp) {
       m_stored_vars.m_set_point = m_max_temp;
-    } else {
-      m_stored_vars.m_set_point += 1;
     }
     store_data();
     return m_stored_vars.m_set_point;
   }
   ///
   /// \brief
-  float incSetPointDoccia() {
+  float incSetPointDoccia(int val=1) {
+    m_stored_vars.m_set_point_doccia += val;
     if (m_stored_vars.m_set_point_doccia >= m_max_temp) {
       m_stored_vars.m_set_point_doccia = m_max_temp;
-    } else {
-      m_stored_vars.m_set_point_doccia += 1;
     }
     store_data();
     return m_stored_vars.m_set_point_doccia;
@@ -81,22 +79,20 @@ public:
 
   ///
   /// \brief
-  float decSetPoint() {
+  float decSetPoint(int val = 1) {
+    m_stored_vars.m_set_point -= val;
     if (m_stored_vars.m_set_point <= m_min_temp) {
       m_stored_vars.m_set_point = m_min_temp;
-    } else {
-      m_stored_vars.m_set_point -= 1;
     }
     store_data();
     return m_stored_vars.m_set_point;
   }
   ///
   /// \brief
-  float decSetPointDoccia() {
+  float decSetPointDoccia(int val = 1) {
+    m_stored_vars.m_set_point_doccia -= val;
     if (m_stored_vars.m_set_point_doccia <= m_min_temp) {
       m_stored_vars.m_set_point_doccia = m_min_temp;
-    } else {
-      m_stored_vars.m_set_point_doccia -= 1;
     }
     store_data();
     return m_stored_vars.m_set_point_doccia;
@@ -165,7 +161,7 @@ private:
   StoreVariables m_stored_vars;  //! Store variables
 
   int m_cnt_store_energy = 0;   //! Counter to store updated energy
-  const float m_max_temp = 55;  //! Massimo setpoint
+  const float m_max_temp = 70;  //! Massimo setpoint
   const float m_min_temp = 5;   //! Minimo setpoint
   const float m_r_pow = 1200;   //! Power consumed by resistance
 };
@@ -177,8 +173,9 @@ private:
 #define RELE_PIN D5     // - RELE
 
 // Dichiarazioni delle costanti per la connessione WiFi
-const char* ssid = "FASTWEB-9n9CGH";
-const char* password = "Su2r9s59Zy";
+const char* ssid[] = { "FASTWEB-9n9CGH", "ivan", NULL };
+const char* password[] = { "Su2r9s59Zy", "ciclamino", NULL };
+
 
 // Dichiarazioni delle costanti per il server webu
 ESP8266WebServer server(80);
@@ -211,28 +208,29 @@ static void reload_page() {
 }
 
 /// \brief Incrementa il setpoint
-void setpointInc() {
-  g_store_data.incSetPoint();
-  reload_page();
-}
+void setpointInc()    { g_store_data.incSetPoint();   reload_page(); }
+void setpointInc_1()  { g_store_data.incSetPoint(1);  reload_page(); }
+void setpointInc_5()  { g_store_data.incSetPoint(5);  reload_page(); }
+void setpointInc_10() { g_store_data.incSetPoint(10); reload_page(); }
 
 /// \brief Decrementa il setpoint
-void setpointDec() {
-  g_store_data.decSetPoint();
-  reload_page();
-}
+void setpointDec()    { g_store_data.decSetPoint();   reload_page(); }
+void setpointDec_1()  { g_store_data.decSetPoint(1);  reload_page(); }
+void setpointDec_5()  { g_store_data.decSetPoint(5);  reload_page(); }
+void setpointDec_10() { g_store_data.decSetPoint(10); reload_page(); }
+
 
 /// \brief Incrementa il setpoint doccia
-void setpointDocciaInc() {
-  g_store_data.incSetPointDoccia();
-  reload_page();
-}
+void setpointDocciaInc()    { g_store_data.incSetPointDoccia();  reload_page(); }
+void setpointDocciaInc_1()  { g_store_data.incSetPointDoccia(1);  reload_page(); }
+void setpointDocciaInc_5()  { g_store_data.incSetPointDoccia(5);  reload_page(); }
+void setpointDocciaInc_10() { g_store_data.incSetPointDoccia(10);  reload_page(); }
 
 /// \brief Decrementa il setpoint doccia
-void setpointDocciaDec() {
-  g_store_data.decSetPointDoccia();
-  reload_page();
-}
+void setpointDocciaDec() { g_store_data.decSetPointDoccia(); reload_page(); }
+void setpointDocciaDec_1() { g_store_data.decSetPointDoccia(1); reload_page(); }
+void setpointDocciaDec_5() { g_store_data.decSetPointDoccia(5); reload_page(); }
+void setpointDocciaDec_10() { g_store_data.decSetPointDoccia(10); reload_page(); }
 
 /// \brief Prenota la doccia
 void prenotaDocciaSet() {
@@ -262,6 +260,126 @@ void toggleDst() {
 
 // Funzione per la gestione della richiesta HTTP GET
 void handleRoot() {
+  String t_acqua_str = String(g_water_temp, 1);
+
+  String t_sp_color = g_is_doccia_scheduled ? "black" : "green";
+  String t_sp_arrow = g_is_doccia_scheduled ? "" : "&rarr; ";
+  String t_set_point = String(g_store_data.getSetPoint(), 1);
+
+  String t_sp_dc_col = g_is_doccia_scheduled ? "green" : "black";
+  String t_sp_dc_arrow = g_is_doccia_scheduled ? "&rarr; " : "";
+  String t_sp_doccia = String(g_store_data.getSetPointDoccia(), 1);
+
+  String doccia_pr_color = g_is_doccia_scheduled ? "red" : "blue";
+  String doccia_pr_txt = g_is_doccia_scheduled ? "Doccia Prenotata" : "Doccia non prenotata";
+  String doccia_pr_but_cb = g_is_doccia_scheduled ? "sch_doccia_clr" : "sch_doccia_set";
+  String doccia_pr_but_txt = g_is_doccia_scheduled ? "Annulla" : "PRENOTA";
+  String str_output = g_out_rele ? "<mark>Acceso</mark>" : "Spento";
+  String butt_dst = g_store_data.isDst() ? "Clear DST" : "Set DST";
+  String str_energy = String((float)g_store_data.getEnergy() / 1000.0f, 2);
+
+  String html2 =
+    ""
+    "<!DOCTYPE html>"
+    "<html lang=\"it\">"
+    "  <head>"
+    "    <meta charset=\"UTF-8\">"
+    "    <title>Boiler del bagno</title>"
+    "    <meta http-equiv=\"refresh\" content=\"5\">"
+    "    <style>"
+    "      table {"
+    "          border-collapse: collapse;"
+    "          border-spacing: 10px;"
+    "          text-align: left;"
+    "          font-size: 2.5vw;"
+    "          width: 100%;"
+    "      }"
+    "      tr {"
+    "          height: 30px;"
+    "      }"
+    "      td {"
+    "          padding: 0px 0px 0px 20px;"
+    "      }"
+    "      td:first-child {"
+    "          padding-left: 0;"
+    "          width: 40%;"
+    "      }"
+    "      .class-temp {"
+    "          font-size: 8vw;"
+    "      }"
+    "      .class-sp {"
+    "          font-size: 3vw;"
+    "      }"
+    "      button {"
+    "          font-size: 3.5vw;"
+    "          padding: 5px 15px;"
+    "	  #          width: 100%;"
+    "	  #          box-sizing: border-box;"
+    "      }"
+    "    </style>"
+    "  </head>"
+    "  <body>"
+    "    <h1>Boiler del bagno</h1>"
+    "    <p>Versione: <b>" VERSION " / " __DATE__ " - " __TIME__ "</b></p>"
+    "    <h2><span class=\"class-temp\">T acqua: " + t_acqua_str + "&deg;C</span></h2>"
+    "    <table>"
+    "      <tr>"
+    "        <td>"
+    "          <h3><span class=\"class-sp\"><font color=\"" + t_sp_color + "\">" + t_sp_arrow + "Set Point : " + t_set_point + "&deg;C</font></span></h3>"
+    "        </td>"
+    "        <td>"
+    "          <button onclick=\"location.href='/setpoint_dec_10'\">- 10</button>"
+    "          <button onclick=\"location.href='/setpoint_dec__5'\">- 5</button>"
+    "          <button onclick=\"location.href='/setpoint_dec__1'\">- 1</button>"
+    "          <button onclick=\"location.href='/setpoint_inc__1'\">+ 1</button>"
+    "          <button onclick=\"location.href='/setpoint_inc__5'\">+ 5</button>"
+    "          <button onclick=\"location.href='/setpoint_inc_10'\">+ 10</button>"
+    "        </td>"
+    "      </tr>"
+    "      <tr>"
+    "        <td>"
+    "          <h3><span class=\"class-sp\"><font color=\"" + t_sp_dc_col + "\">" + t_sp_dc_arrow + "Set Point Doccia: " + t_sp_doccia + "&deg;C</font></span></h3>"
+    "        </td>"
+    "        <td>"
+    "          <button onclick=\"location.href='/sp_doccia_dec_10'\">- 10</button>"
+    "          <button onclick=\"location.href='/sp_doccia_dec__5'\">- 5</button>"
+    "          <button onclick=\"location.href='/sp_doccia_dec__1'\">- 1</button>"
+    "          <button onclick=\"location.href='/sp_doccia_inc__1'\">+ 1</button>"
+    "          <button onclick=\"location.href='/sp_doccia_inc__5'\">+ 5</button>"
+    "          <button onclick=\"location.href='/sp_doccia_inc_10'\">+ 10</button>"
+    "        </td>"
+    "      </tr>"
+    "    </table>"
+    "    <hr>"
+    "    <table>"
+    "      <tr>"
+    "	       <td><p><font color=\"" + doccia_pr_color + "\">" + doccia_pr_txt + "</font></p></td>"
+    "	       <td><button onclick=\"location.href='/" + doccia_pr_but_cb + "'\">" + doccia_pr_but_txt + "</button></td>"
+    "      </tr>"
+    "    </table>"
+    "    <hr>"
+    "    <table>"
+    "      <tr>"
+    "	       <td><p><font color=\"black\">OUTPUT:</font></p></td>"
+    "	       <td><p><font color=\"black\">" + str_output + "</font></p></td>"
+    "      </tr>"
+    "    </table>"
+    "    <hr>"
+    "    <table>"
+    "      <tr>"
+    "        <td><b>Ora: " + g_web_time.getDateTime() + "</b></td>"
+    "        <td><button onclick=\"location.href='/toggle_dst'\">" + butt_dst + "</button></td>"
+    "      </tr>"
+    "      <tr>"
+    "	<td><h3>Energia: " + str_energy + "kWh</h3></td>"
+    "	<td><button onclick=\"location.href='/reset_energy'\">RESET</button></td>"
+    "      </tr>"
+    "    </table>"
+    "  </body>"
+    "</html>"
+
+    ;
+
   String html = "<html>"
                 "<head>"
                 "<title>Boiler del bagno</title>"
@@ -284,26 +402,25 @@ void handleRoot() {
                 "  td:first-child {"
                 "    padding-left: 0;"
                 "  }"
+                "  .class-temp {"
+                "    font-size: 200px;"
+                "  }"
                 "</style>"
                 "</head>"
                 "<body>"
-                "<h1>Boiler del bagno</h1>";
+                "<h1>Boiler del bagno</h1>"
+                "<p>Versione: <b>" VERSION " / " __DATE__ " - " __TIME__ "</b></p>";
 
-  html += "<table><tr>";
-  html += "<td><b>Ora: " + g_web_time.getDateTime() + "</b></td>";
-  if (g_store_data.isDst()) {
-    html += "<td><button onclick=\"location.href='/toggle_dst'\">Clear DST</button></td>";
-  } else {
-    html += "<td><button onclick=\"location.href='/toggle_dst'\">Set DST</button></td>";
-  }
-  html += "</tr></table>";
-
+  //-----------------------------------------------------------------------------------------------
   // Temperatura
+  //-----------------------------------------------------------------------------------------------
   char txt[64];
   dtostrf(g_water_temp, 4, 1, txt);
-  html += "<h2>T acqua  : " + String(txt) + "&deg;C</h2>";
+  html += "<h2><span class=\".class-temp\">T acqua  : " + String(txt) + "&deg;C</span></h2>";
 
+  //-----------------------------------------------------------------------------------------------
   // Tasti set point
+  //-----------------------------------------------------------------------------------------------
   dtostrf(g_store_data.getSetPoint(), 4, 1, txt);
   if (!g_is_doccia_scheduled) {
     html += "<h3><font color=\"green\">&rarr;";
@@ -314,7 +431,9 @@ void handleRoot() {
   html += "<button onclick=\"location.href='/setpoint_dec'\">- 1</button>";
   html += "<button onclick=\"location.href='/setpoint_inc'\">+ 1</button>";
 
+  //-----------------------------------------------------------------------------------------------
   // Tasti set point Doccia
+  //-----------------------------------------------------------------------------------------------
   dtostrf(g_store_data.getSetPointDoccia(), 4, 1, txt);
   if (g_is_doccia_scheduled) {
     html += "<h3><font color=\"green\">&rarr;";
@@ -325,7 +444,9 @@ void handleRoot() {
   html += "<button onclick=\"location.href='/sp_doccia_dec'\">- 1</button>";
   html += "<button onclick=\"location.href='/sp_doccia_inc'\">+ 1</button></h3>";
 
+  //-----------------------------------------------------------------------------------------------
   // Tasti set prenotare la doccia
+  //-----------------------------------------------------------------------------------------------
   if (g_is_doccia_scheduled) {
     html += "<hr><h3><font color=\"red\">Doccia Prenotata   </font>";
     html += "<button onclick=\"location.href='/sch_doccia_clr'\">Annulla</button></h3>";
@@ -334,29 +455,67 @@ void handleRoot() {
     html += "<button onclick=\"location.href='/sch_doccia_set'\">PRENOTA</button></h3>";
   }
 
-  // Led
+  //-----------------------------------------------------------------------------------------------
+  // Stato uscita
+  //-----------------------------------------------------------------------------------------------
   html += "<hr><p>OUTPUT: ";
   html += g_out_rele ? "<mark>Acceso</mark></p>" : "Spento</p><hr>";
 
+  //-----------------------------------------------------------------------------------------------
+  // ORA
+  //-----------------------------------------------------------------------------------------------
+  html += "<hr><table><tr>";
+  html += "<td><b>Ora: " + g_web_time.getDateTime() + "</b></td>";
+  if (g_store_data.isDst()) {
+    html += "<td><button onclick=\"location.href='/toggle_dst'\">Clear DST</button></td>";
+  } else {
+    html += "<td><button onclick=\"location.href='/toggle_dst'\">Set DST</button></td>";
+  }
+  html += "</tr></table>";
+
+  //-----------------------------------------------------------------------------------------------
   // Power consumption
+  //-----------------------------------------------------------------------------------------------
   dtostrf((float)g_store_data.getEnergy() / 1000.0f, 4, 2, txt);
   html += "<h3>Energia consumata : " + String(txt) + "kWh    ";
   html += "<button onclick=\"location.href='/reset_energy'\">RESET</button>";
 
-  server.send(200, "text/html", html);
-  server_ext.send(200, "text/html", html);
+
+  server.send(200, "text/html", html2);
+  server_ext.send(200, "text/html", html2);
 }
 
 
 static bool handle_connection() {
   // Gestione connessione
   static bool connected = false;
+  static int i_conn = 0;
+  static int retry = 0;
+
+  if (!connected) {
+    if (--retry <= 0) {
+      retry = 10;
+      if (ssid[i_conn] == NULL) {
+        i_conn = 0;
+      }
+      WiFi.begin(ssid[i_conn], password[i_conn]);
+      Serial.print("Rete: ");
+      Serial.println(ssid[i_conn]);
+      ++i_conn;
+    }
+  }
 
   if (WiFi.status() != WL_CONNECTED) {
+    if (connected) {
+      // Era connesso
+      i_conn = 0;
+      retry = 0;
+    }
     connected = false;
     Serial.println("Connessione in corso...");
   } else {
     if (!connected) {
+      // Alla connessione
       // Stampa l'indirizzo IP sulla seriale
       Serial.print("Indirizzo IP: ");
       Serial.println(WiFi.localIP());
@@ -428,7 +587,8 @@ static void ctrl_led() {
     }
   }
 
-  digitalWrite(LED_PIN, led_phase);
+  //digitalWrite(LED_PIN, led_phase);
+  analogWrite(LED_PIN, led_phase ? 64 : 0);
 }
 
 void setup() {
@@ -439,7 +599,8 @@ void setup() {
 
   // Inizializzazione del LED
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  //digitalWrite(LED_PIN, LOW);
+  analogWrite(LED_PIN, 0);
   g_led_ticker.attach_ms(10, ctrl_led);
 
   // Inizializzazione uscita rele
@@ -448,10 +609,27 @@ void setup() {
 
   // Impostazione dei gestori per le richieste HTTP
   server.on("/", handleRoot);
+
   server.on("/setpoint_inc", setpointInc);
+  server.on("/setpoint_inc_10", setpointInc_10);
+  server.on("/setpoint_inc__5", setpointInc_5);
+  server.on("/setpoint_inc__1", setpointInc_1);
+
   server.on("/setpoint_dec", setpointDec);
-  server.on("/sp_doccia_inc", setpointDocciaInc);
-  server.on("/sp_doccia_dec", setpointDocciaDec);
+  server.on("/setpoint_dec_10", setpointDec_10);
+  server.on("/setpoint_dec__5", setpointDec_5);
+  server.on("/setpoint_dec__1", setpointDec_1);
+
+  server.on("/sp_doccia_inc"   , setpointDocciaInc);
+  server.on("/sp_doccia_inc_10", setpointDocciaInc_10);
+  server.on("/sp_doccia_inc__5", setpointDocciaInc_5);
+  server.on("/sp_doccia_inc__1", setpointDocciaInc_1);
+
+  server.on("/sp_doccia_dec"   , setpointDocciaDec);
+  server.on("/sp_doccia_dec_10", setpointDocciaDec_10);
+  server.on("/sp_doccia_dec__5", setpointDocciaDec_5);
+  server.on("/sp_doccia_dec__1", setpointDocciaDec_1);
+
   server.on("/sch_doccia_set", prenotaDocciaSet);
   server.on("/sch_doccia_clr", prenotaDocciaClr);
   server.on("/reset_energy", resetEnergy);
@@ -471,8 +649,8 @@ void setup() {
   server.begin();
   server_ext.begin();
 
-  // Connessione alla rete WiFi
-  WiFi.begin(ssid, password);
+  // // Connessione alla rete WiFi
+  // WiFi.begin(ssid, password);
 
   // Imposta DST
   g_web_time.setDst(g_store_data.isDst());
